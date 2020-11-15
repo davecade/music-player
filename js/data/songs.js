d4d290da3c4817cd4a3c252ca6f0fd1713d87521
Dave <dave.cadelina@outlook.com>
2020-11-16 00:24:36 +1100

    added songs list

---
+++ js/data/songs.js
@@ -0,0 +1,85 @@
+export const songsList = [{
+    "id": "1",
+    "url": "../../music/STNDRD - My Uso (Official Audio).mp3",
+    "time": "04:10",
+    "title": "My Uso",
+    "artist": "STNDRD"
+  },
+  {
+    "id": "2",
+    "url": "../../music/Constant - Jeremy Passion & Jesse Barrera.mp3",
+    "time": "03:11",
+    "title": "Constant",
+    "artist": "Passion"
+  },
+  {
+    "id": "3",
+    "url": "../../music/Camila Cabello - Havana ft. Young Thug - Lyrics.mp3",
+    "time": "03:37",
+    "title": "Havana",
+    "artist": "Camila Cabello"
+  },
+  {
+    "id": "4",
+    "url": "../../music/Jason_Derulo_It_Girl_Official_Video.mp3",
+    "time": "03:33",
+    "title": "It Girl",
+    "artist": "Jason Derulo"
+  },
+  {
+    "id": "5",
+    "url": "../../music/Jesse Barrera - Carousel.mp3",
+    "time": "03:19",
+    "title": "Carousel",
+    "artist": "Jesse Barrera"
+  },
+  {
+    "id": "6",
+    "url": "../../music/Omarion - I'm Tryna.mp3",
+    "time": "04:00",
+    "title": "I'm Tryna",
+    "artist": "Omarion"
+  },
+  {
+    "id": "7",
+    "url": "../../music/P_Y_T_Pretty_Young_Thing_Michael_Jackson.mp3",
+    "time": "03:58",
+    "title": "PYT",
+    "artist": "Michael Jackson"
+  },
+  {
+    "id": "8",
+    "url": "../../music/Billy Joel - The Longest Time (Audio).mp3",
+    "time": "03:44",
+    "title": "The Longest Time",
+    "artist": "Billy Joel"
+  },
+  {
+    "id": "9",
+    "url": "../../music/Tyrese - How You Gonna Act Like That.mp3",
+    "time": "04:07",
+    "title": "How You Gonna Act Like That",
+    "artist": "Tyrese"
+  },
+  {
+    "id": "10",
+    "url": "../../music/Janet_Jackson_Escapade.mp3",
+    "time": "04:46",
+    "title": "Escapade",
+    "artist": "Jant Jackson"
+  },
+  {
+    "id": "11",
+    "url": "../../music/Janet_Jackson_That_s_the_way_love_goes_1993.mp3",
+    "time": "04:30",
+    "title": "That's the way love goes",
+    "artist": "Janet Jackson"
+  },
+  {
+    "id": "12",
+    "url": "../../music/Nick Jonas - Jealous.mp3",
+    "time": "03:42",
+    "title": "Jealous",
+    "artist": "Nick Jonas"
+  },
+]
