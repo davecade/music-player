2b4fd2e38eeb3fbaa92a189b9510af17ecb36d94
Dave <dave.cadelina@outlook.com>
2020-11-16 23:17:39 +1100

    added toggle play and pause when icon is clicked

--- js/modules/playlist.js
+++ js/modules/playlist.js
@@ -6,21 +6,93 @@ const Playlist = (() => {
 
     // -- Set State
     const state = {
+        currentlyPlayingIndex: 0,
         isPlaying: false
     }
 
+    // -- Variables
+    let songs = songsList;
+    let currentSong = new Audio(songs[state.currentlyPlayingIndex].url)
+
+    // -- Event Listeners
+    const eventListeners = () => {
+        playlistEl.addEventListener('click', (event) => {
+            if(event.target.matches(".fas")) {
+                const songClicked = event.target.parentNode;
+                const indexOfSongClicked = [...event.target.parentNode.parentNode.children].indexOf(songClicked);
+                handlePlayPause(indexOfSongClicked)
+            }
+        })
+    }
+
+    // -- Methods
     const setState = obj => {
         state.isPlaying = obj.isPlaying
+    }
+
+    const handlePlayPause = (index) => {
+        if(state.currentlyPlayingIndex === index) {
+            togglePlayPause();
+        } else {
+            updateCurrentlyPlayingIndex(index)
+            changeSongSrc();
+            togglePlayPause();
+        }
+        render();
+    }
+
+    const togglePlayPause = () => {
+        if(currentSong.paused) {
+            currentSong.play()
+            state.isPlaying = true;
+        } else {
+            currentSong.pause();
+            state.isPlaying = false;
+        }
+    }
+
+    const changeSongSrc = () => {
+        currentSong = new Audio(songs[state.currentlyPlayingIndex].url)
+    }
+
+    const updateCurrentlyPlayingIndex = (newIndex) => {
+        state.currentlyPlayingIndex = newIndex
     }
 
     // -- Initialize
     const init = () => {
         render();
+        eventListeners();
     }
 
     //-- Render the DOM
     const render = () => {
         let markup = '';
+
+        const togglePlayPauseIcon = (index) => {
+            if(index === state.currentlyPlayingIndex) {
+                return currentSong.paused ? 'play' : 'pause';
+            } else {
+                return 'play'
+            }
+        }
+
+        songs.forEach((obj, index) => {
+            markup += `
+                <li class="playlist-item">
+                    <i class="fas fa-${togglePlayPauseIcon(index)}"></i>
+                    <div class="song-info">
+                        <div>
+                            <p class="song-title">${obj.title}</p>
+                            <p class="song-artist">${obj.artist}</p>
+                        </div>
+                        <p class="song-duration">${obj.time}</p>
+                    </div>
+                </li>
+            `
+        })
+
+        playlistEl.innerHTML = markup;
     }
 
     // -- Public Methods
